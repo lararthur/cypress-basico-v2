2822c342f9b04c50ca4767bfef804247edd11f25
lararthur <alara@redventures.com>
2022-03-20 16:52:20 -0300

    exercício

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -2,10 +2,19 @@
 /// <reference types="Cypress" />
 
 describe('Central de Atendimento ao Cliente TAT', function() {
-  it('verifica o título da aplicação', function() {
+  beforeEach(() => {
     cy.visit('../../src/index.html')
+  })
+  it('verifica o título da aplicação', function() {
     cy.title()
       .should('eq', 'Central de Atendimento ao Cliente TAT')
-      // .should('be.equal', 'Central de Atendimento ao Cliente TAT')
+  })
+  it('preenche os campos obrigatórios e envia o formulário', function() {
+    cy.get('#firstName').type('Garibaldo')
+    cy.get('#lastName').type('de Paula')
+    cy.get('#email').type('gpaula@gmail.com')
+    cy.get('#open-text-area').type('Testando...')
+    cy.get('button[type=submit]').click()
+    cy.get('.success').should('be.visible')
   })
 })
